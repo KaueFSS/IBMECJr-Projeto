f4f38a9d16baa2c6be2818ac9d949e292565e288
Bx-dev-2402 <bernardobrandao252@gmail.com>
2026-04-22 16:06:22 -0300

    Criação da Database

--- sistema_estoque/mercadinho/admin.py
+++ sistema_estoque/mercadinho/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import (
+    Cliente,
+    Fornecedor,
+    Funcionario,
+    Produto,
+    CompraFornecedor,
+    Despesa,
+    Estoque,
+    Venda,
+    ItemCompra,
+    ItemVenda,
+)
 
-# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Fornecedor)
+admin.site.register(Funcionario)
+admin.site.register(Produto)
+admin.site.register(CompraFornecedor)
+admin.site.register(Despesa)
+admin.site.register(Estoque)
+admin.site.register(Venda)
+admin.site.register(ItemCompra)
+admin.site.register(ItemVenda)
