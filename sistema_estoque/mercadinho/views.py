59f3750fd61e34b3317948fa68aafda83de8677f
Micael Dali <dali.micael@gmail.com>
2026-04-26 11:00:42 -0300

    criação dos viewsets de produto e estoque, e criação da verificação de alerta de itens com estoque abaixo do minimo, como extensao do viewset de estoque.

--- sistema_estoque/mercadinho/views.py
+++ sistema_estoque/mercadinho/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.db.models import F
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Produto, Estoque
+from .serializers import ProdutoSerializer, EstoqueSerializer
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+class EstoqueViewSet(viewsets.ModelViewSet):
+    queryset = Estoque.objects.select_related('produto').all()
+    serializer_class = EstoqueSerializer
+
+    @action(detail=False, methods=['get'], url_path='alerta-minimo')
+    def alerta(self, request):
+        estoques_alerta = Estoque.objects.filter(
+            quantidade_atual__lt = F("estoque_minimo")
+        )
+
+        serializer = self.get_serializer(estoques_alerta, many=True)
+
+        return Response({
+            'total': itens_em_alerta.count(),
+            'resultados': serializer.data
+        })
+        
